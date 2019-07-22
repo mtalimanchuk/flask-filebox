a7d784fd2ca508d48f0b3ff69cb6148fb443a694
mtalimanchuk <mtalimanchuk@gmail.com>
2019-07-23 02:06:48 +0300

    Add config, update flash messages

--- filebox.py
+++ filebox.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3.7
+import logging
 from pathlib import Path
+
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = Path("box")
-# FIXME substitute "box" with either complete or relative file path that works for you
-UPLOAD_FOLDER.mkdir(exist_ok=True)
-ALLOWED_EXTENSIONS = {
-    'txt',
-    'pdf',
-    'png',
-    'jpg',
-    'jpeg',
-    'gif'
-    }
-# FIXME revise allowed extensions
+from config import UPLOAD_FOLDER, ALLOWED_EXTENSIONS, SECRET_KEY
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "super secret key"
-# FIXME change your secret_key. It can be any string (or maybe even something else)
+logging.basicConfig(level=logging.INFO)
+app.config['UPLOAD_FOLDER'] = Path(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'].mkdir(exist_ok=True)
+app.secret_key = SECRET_KEY
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -28,19 +20,24 @@ app.secret_key = "super secret key"
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file in request')
+            flash('Select the file to upload', 'action-fail')
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
-            flash('No selected file')
+            flash('Empty filename', 'action-fail')
             return redirect(request.url)
-        if file and Path(file.filename).suffix.strip('.') in ALLOWED_EXTENSIONS:
+        file_extension = Path(file.filename).suffix.strip('.')
+        if file and file_extension in ALLOWED_EXTENSIONS:
             filename = secure_filename(file.filename)
             full_dir_path = app.config['UPLOAD_FOLDER'] / request.form['path']
             full_dir_path.mkdir(exist_ok=True)
             full_file_path = full_dir_path / filename
             file.save(str(full_file_path))
-            flash(f"{full_file_path} uploaded successfully")
+            app.logger.info(f"Uploaded {full_file_path}")
+            flash(f"{full_file_path} uploaded successfully", 'action-success')
+        else:
+            flash(f"{file_extension} files are not allowed", 'action-fail')
+            return redirect(request.url)
 
     return render_template("index.html.j2",
                             upload_folder=str(app.config['UPLOAD_FOLDER']),
