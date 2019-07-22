a5cc58f72f5c09b040f4e73d22c3a47e9481bf7c
mtalimanchuk <mtalimanchuk@gmail.com>
2019-07-22 23:45:34 +0300

    Add nested folder support, update template accordingly

--- filebox.py
+++ filebox.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = Path("box")
+# FIXME substitute "box" with either complete or relative file path that works for you
 UPLOAD_FOLDER.mkdir(exist_ok=True)
 ALLOWED_EXTENSIONS = {
     'txt',
@@ -13,10 +14,12 @@ ALLOWED_EXTENSIONS = {
     'jpeg',
     'gif'
     }
+# FIXME revise allowed extensions
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "super secret key"
+# FIXME change your secret_key. It can be any string (or maybe even something else)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -25,7 +28,7 @@ app.secret_key = "super secret key"
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file in request')
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
@@ -33,10 +36,15 @@ def upload_file():
             return redirect(request.url)
         if file and Path(file.filename).suffix.strip('.') in ALLOWED_EXTENSIONS:
             filename = secure_filename(file.filename)
-            file.save(str(app.config['UPLOAD_FOLDER'] / filename))
-            flash(f"{filename} uploaded successfully")
+            full_dir_path = app.config['UPLOAD_FOLDER'] / request.form['path']
+            full_dir_path.mkdir(exist_ok=True)
+            full_file_path = full_dir_path / filename
+            file.save(str(full_file_path))
+            flash(f"{full_file_path} uploaded successfully")
 
-    return render_template("index.html.j2")
+    return render_template("index.html.j2",
+                            upload_folder=str(app.config['UPLOAD_FOLDER']),
+                            existing_dirs=["/".join(d.parts[1:]) for d in app.config['UPLOAD_FOLDER'].rglob('*') if d.is_dir()])
 
 
 if __name__ == '__main__':
