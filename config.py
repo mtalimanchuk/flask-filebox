1d150bb2ac99467db1cdabab864a43eed27484b2
mtalimanchuk <mtalimanchuk@gmail.com>
2019-07-23 23:55:48 +0300

    Remove default storage dir, remove file extension check

--- config.py
+++ config.py
@@ -1,13 +1,2 @@
-UPLOAD_FOLDER = "box"
-# FIXME root upload folder. Replace with any complete or relative directory path.
-ALLOWED_EXTENSIONS = {
-    'txt',
-    'pdf',
-    'png',
-    'jpg',
-    'jpeg',
-    'gif'
-    }
-# FIXME revise allowed extensions
 SECRET_KEY = "super secret key"
 # FIXME change your secret_key. It can be any string (or maybe even something else)
--- filebox.py
+++ filebox.py
@@ -5,12 +5,10 @@ from pathlib import Path
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-from config import UPLOAD_FOLDER, ALLOWED_EXTENSIONS, SECRET_KEY
+from config import SECRET_KEY
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
-app.config['UPLOAD_FOLDER'] = Path(UPLOAD_FOLDER)
-app.config['UPLOAD_FOLDER'].mkdir(exist_ok=True)
 app.secret_key = SECRET_KEY
 
 
@@ -26,22 +24,19 @@ def upload_file():
         if file.filename == '':
             flash('Empty filename', 'action-fail')
             return redirect(request.url)
-        file_extension = Path(file.filename).suffix.strip('.')
-        if file and file_extension in ALLOWED_EXTENSIONS:
+        if file:
             filename = secure_filename(file.filename)
-            full_dir_path = app.config['UPLOAD_FOLDER'] / request.form['path']
-            full_dir_path.mkdir(exist_ok=True)
-            full_file_path = full_dir_path / filename
+            upload_dir_path = Path(request.form['path'])
+            upload_dir_path.mkdir(parents=True, exist_ok=True)
+            full_file_path = upload_dir_path / filename
             file.save(str(full_file_path))
             app.logger.info(f"Uploaded {full_file_path}")
             flash(f"{full_file_path} uploaded successfully", 'action-success')
         else:
-            flash(f"{file_extension} files are not allowed", 'action-fail')
+            flash(f"No file", 'action-fail')
             return redirect(request.url)
 
-    return render_template("index.html.j2",
-                            upload_folder=str(app.config['UPLOAD_FOLDER']),
-                            existing_dirs=["/".join(d.parts[1:]) for d in app.config['UPLOAD_FOLDER'].rglob('*') if d.is_dir()])
+    return render_template("index.html.j2")
 
 
 if __name__ == '__main__':
