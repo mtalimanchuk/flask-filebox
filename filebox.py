b0de2293ea95ae817a1b338b3bd268307e91fc4c
mtalimanchuk <mtalimanchuk@gmail.com>
2021-03-25 01:57:42 +0300

    add file download, rework frontend

--- filebox.py
+++ filebox.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3.7
+import io
 import logging
 from pathlib import Path
+import tarfile
+import zipfile
 
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, jsonify, send_file, Response
 from werkzeug.utils import secure_filename
 
 from config import SECRET_KEY
@@ -13,32 +16,149 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['LAST_USED_DIR'] = None
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('Select the file to upload', 'action-fail')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('Empty filename', 'action-fail')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            upload_dir_path = Path(request.form['path'])
-            upload_dir_path.mkdir(parents=True, exist_ok=True)
-            full_file_path = upload_dir_path / filename
-            file.save(str(full_file_path))
-            app.config['LAST_USED_DIR'] = str(upload_dir_path)
-            app.logger.info(f"Uploaded {full_file_path}")
-            flash(f"{full_file_path} uploaded successfully", 'action-success')
-        else:
-            flash(f"No file", 'action-fail')
-            return redirect(request.url)
+def _add_to_archive(add_f: callable, path: Path):
+    arcname_offset = len(path.parents)
+    if path.is_dir():
+        for f_name in path.glob("**/*"):
+            add_f(f_name, Path(*f_name.parts[arcname_offset:]))
+    else:
+        add_f(path, Path(*path.parts[arcname_offset:]))
 
-    return render_template("index.html.j2", last_used_dir=app.config['LAST_USED_DIR'])
+
+def _make_zip(path):
+    path = Path(path)
+    data = io.BytesIO()
+    with zipfile.ZipFile(data, mode='w') as z:
+        _add_to_archive(z.write, path)
+    data.seek(0)
+
+    return data, 'application/zip', f'{path.stem}.zip'
+
+
+def _make_tar(path):
+    path = Path(path)
+    data = io.BytesIO()
+    with tarfile.TarFile(fileobj=data, mode='w') as tar:
+        _add_to_archive(tar.add, path)
+    data.seek(0)
+
+    return data, 'application/tar', f'{path.stem}.tar.gz'
+
+
+def serve_file(path, ftype):
+    if ftype == "zip":
+        data, mimetype, fname = _make_zip(path)
+    elif ftype == "tar":
+        data, mimetype, fname = _make_tar(path)
+    else:
+        raise ValueError(f"Wrong file type {ftype}")
+
+    return data, mimetype, fname
+
+
+def save_file(files, save_dir):
+    if 'file' not in request.files:
+        raise FileNotFoundError('Select the file to upload')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        raise ValueError('Empty filename')
+
+    if not file:
+        raise ValueError('Empty file')
+
+    filename = secure_filename(file.filename)
+
+    upload_dir_path = Path(save_dir).absolute()
+    upload_dir_path.mkdir(parents=True, exist_ok=True)
+
+    full_file_path = upload_dir_path / filename
+    file.save(str(full_file_path))
+
+    app.config['LAST_USED_DIR'] = str(upload_dir_path)
+    app.logger.info(f"Uploaded {full_file_path}")
+
+    return full_file_path
+
+
+def dir_info(path):
+    path = Path(path).absolute()
+    dir_exists = False
+    breadcrumbs = []
+    content = []
+
+    for i, part in enumerate(path.parts):
+        p = Path(*path.parts[:i + 1])
+        item = {
+            "name": part,
+            "absolute": str(p.absolute()),
+        }
+        breadcrumbs.append(item)
+
+    if path.is_dir():
+        dir_exists = True
+        for c in path.glob("*"):
+            item = {
+                "name": c.name,
+                "absolute": str(c.absolute()),
+                "is_dir": c.is_dir(),
+            }
+            content.append(item)
+
+    response = {
+        "exists": dir_exists,
+        "breadcrumbs": breadcrumbs,
+        "parent": str(path.parent),
+        "content": content,
+    }
+
+    return response
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    if request.method == 'POST':
+        app.logger.info(request.files)
+        try:
+            files = request.files
+            save_dir = request.form['path']
+            full_file_path = save_file(files, save_dir)
+            flash(f"{full_file_path} uploaded successfully", 'action-success')
+        except (FileNotFoundError, ValueError) as e:
+            flash(str(e), 'action-fail')
+
+        return redirect(request.url)
+
+    return render_template("index.html", last_used_dir=app.config['LAST_USED_DIR'])
+
+
+@app.route('/api/path', methods=["POST"])
+def resolve_path():
+    path = request.json
+
+    response = dir_info(path)
+
+    return jsonify(response)
+
+
+@app.route('/api/download', methods=['GET'])
+def download():
+    try:
+        path = request.args["path"]
+        ftype = request.args["ftype"]
+
+        data, mimetype, fname = serve_file(path, ftype)
+        return send_file(
+            data,
+            mimetype=mimetype,
+            as_attachment=True,
+            attachment_filename=fname
+        )
+    except KeyError as e:
+        return Response(str(e), status=404)
+    except ValueError as e:
+        return Response(str(e), status=404)
 
 
 if __name__ == '__main__':
